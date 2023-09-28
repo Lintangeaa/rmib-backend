03194bead37686960282c2ef3488d347a06112da
Lintangeaa <lindprak@gmail.com>
2023-09-28 19:34:06 +0700

    feature: get all contact api

--- controllers/learnController.js
+++ controllers/learnController.js
@@ -2,6 +2,21 @@ const { Contact } = require("../db/models")
 const catchAsync = require("../utils/catchAsync")
 
 module.exports = {
+  getAllContact: catchAsync(async (req, res) => {
+    const data = await Contact.findAll()
+
+    if (data.length == 0) {
+      res.status(200).json({
+        status: false,
+        message: "Data tidak ada!",
+      })
+    }
+
+    res.status(200).json({
+      status: true,
+      data,
+    })
+  }),
   addContact: catchAsync(async (req, res) => {
     const { name, email, subject, message } = req.body
 
--- routes/contact.js
+++ routes/contact.js
@@ -1,8 +1,9 @@
 var express = require("express")
-const { addContact } = require("../controllers/learnController")
+const { addContact, getAllContact } = require("../controllers/learnController")
 
 var router = express.Router()
 
 router.post("/", addContact)
+router.get("/", getAllContact)
 
 module.exports = router
